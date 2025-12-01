b24a6b993c39387d6fd820f79c05ff273fd3ba3a
ronaldordz7 <ronaldorodriguezacevedo7@gmail.com>
2025-12-01 01:40:01 -0500

    adasd

--- lib/image-config.ts
+++ lib/image-config.ts
@@ -1,15 +1,15 @@
 export const imageConfig = {
   // Dish images - Replace these URLs with your public image links
   dishes: {
-    buddhaBowl: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&h=500&fit=crop",
+    buddhaBowl: "https://cdn.loveandlemons.com/wp-content/uploads/2020/06/IMG_25462-580x791.jpg",
     greenWrap: "https://tofuu.getjusto.com/orioneat-local/resized2/DC7StBC7JLE5Zwz2P-300-x.webp",
-    kaleSalad: "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?w=500&h=500&fit=crop",
-    smoothie: "https://images.unsplash.com/photo-1587239541167-1aa9268dd3f9?w=500&h=500&fit=crop",
-    troicalBowl: "https://images.unsplash.com/photo-1590080876614-fef1ef8f9e0a?w=500&h=500&fit=crop",
-    causeaLimena: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&h=500&fit=crop",
-    cevicheBowl: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&h=500&fit=crop",
-    potatoPurple: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&h=500&fit=crop",
-    energySmoothie: "https://images.unsplash.com/photo-1587239541167-1aa9268dd3f9?w=500&h=500&fit=crop",
+    kaleSalad: "https://cdn.loveandlemons.com/wp-content/uploads/2019/01/IMG_15972-crop2-580x795.jpg",
+    smoothie: "https://www.gastrolabweb.com/u/fotografias/m/2021/6/12/f1456x819-14666_185036_5050.jpeg",
+    troicalBowl: "https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480/img/recipe/ras/Assets/F20655FC-2D93-46B1-85C9-39700E59554E/Derivates/D82DA87E-116F-4B0D-AA16-E64362A4FC45.jpg",
+    causeaLimena: "https://www.agroperu.pe/wp-content/uploads/2025/02/agroperu-informa_semana-causa-limena-peruana.jpg",
+    cevicheBowl: "https://popmenucloud.com/cdn-cgi/image/width%3D1920%2Cheight%3D1920%2Cfit%3Dscale-down%2Cformat%3Dauto%2Cquality%3D60/vzcrywde/7ddf1d2e-ce89-4ef0-9d74-4181d417d69e.jpg",
+    potatoPurple: "https://foodnheal.com/wp-content/uploads/2020/06/purple-potato-recipe.jpg",
+    energySmoothie: "https://foodnheal.com/wp-content/uploads/2020/06/purple-potato-recipe.jpg",
   },
 
   // Logo - Change to your logo URL
